48961d36ff798660849633eba4b3efd6121d22a4
Kevin Conner <connerk@gmail.com>
2021-03-07 22:52:02 -0500

    Work through some of ziglearn.org
    Bookmark: https://ziglearn.org/chapter-1/#functions

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,88 @@
 const std = @import("std");
 
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+    std.debug.print("Hello, {s}!\n", .{"World"});
+}
+
+const expect = std.testing.expect;
+
+test "if statement" {
+    const a = true;
+    var x: u16 = 0;
+
+    if (a) {
+        x += 1;
+    } else {
+        x += 2;
+    }
+
+    expect(x == 1);
+}
+
+test "if statement expression" {
+    const a = true;
+    var x: u16 = 0;
+    x += if (a) 1 else 2;
+    expect(x == 1);
+}
+
+test "while" {
+    var i: u8 = 2;
+
+    while (i < 100) {
+        i *= 2;
+    }
+
+    expect(i == 128);
+}
+
+test "while with continue expression" {
+    var sum: u8 = 0;
+    var i: u8 = 1;
+
+    while (i <= 10) : (i += 1) {
+        sum += i;
+    }
+
+    expect(sum == 55);
+}
+
+test "while with continue" {
+    var sum: u8 = 0;
+    var i: u8 = 0;
+
+    while (i <= 3) : (i += 1) {
+        if (i == 2) {
+            continue;
+        }
+
+        sum += i;
+    }
+
+    expect(sum == 4);
+}
+
+test "while with break" {
+    var sum: u8 = 0;
+    var i: u8 = 0;
+
+    while (i <= 3) : (i += 1) {
+        if (i == 2) {
+            break;
+        }
+
+        sum += i;
+    }
+
+    expect(sum == 1);
+}
+
+test "for" {
+    // character literals are equivalent to integer literals
+    const string = [_]u8{ 'a', 'b', 'c' };
+
+    for (string) |character, index| {}
+    for (string) |character| {}
+    for (string) |_, index| {}
+    for (string) |_| {}
 }
